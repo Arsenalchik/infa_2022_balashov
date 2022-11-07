2abd5011dface68c1879fa4b21c120e64715f63e
Arsenalchik <117148436+Arsenalchik@users.noreply.github.com>
2022-11-07 21:56:23 +0300

    Update igorr.py

--- igorr.py
+++ igorr.py
@@ -1,19 +1,26 @@
 import pygame
 from pygame.draw import *
 
+import pygame
+from pygame.draw import *
+
 pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+rect(screen, (100, 0, 100), (0, 0, 400, 400))
+circle(screen, (255, 255, 0), (200, 200), 110)
+circle(screen, (0, 0, 0), (200, 200), 110, 2)
+rect(screen, (0, 0, 0), (150, 260, 100, 22))
+circle(screen, (255, 0, 0), (150, 180), 20)
+circle(screen, (0, 0, 0), (150, 180), 8)
+circle(screen, (255, 0, 0), (250, 180), 16)
+circle(screen, (0, 0, 0), (250, 180), 8)
+polygon(screen, (0, 0, 0), [(100, 100), (108,92),
+                               (176,160), (168, 170)])
+polygon(screen, (0, 0, 0), [(233, 173), (225, 163), (323, 103), (330,113)])
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -26,6 +33,7 @@ while not finished:
             finished = True
 
 pygame.quit()
+
 
 
 
